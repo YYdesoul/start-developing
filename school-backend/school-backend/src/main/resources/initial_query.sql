40f877fb594eaeb46ee7c4e38d1e09816b7e44ad
wubingting <3403774803@qq.com>
2022-04-13 21:30:18 +0200

    add school module

---
+++ school-backend/school-backend/src/main/resources/initial_query.sql
@@ -0,0 +1,10 @@
+create table student (
+                         id int primary key,
+                         name text,
+                         gender text,
+                         score_id int
+);
+
+insert into student values (1, 'xiaoming', 'male', 1);
+insert into student values (2, 'xiaohong', 'female', 2);
+insert into student values (3, 'xiaofang', 'female', 3);
